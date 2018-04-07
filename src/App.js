e06d8b0fd52b7ad70616490eeb4f9027a572ba8d
cparki3 <parkinsonchris_76@hotmail.com>
2018-04-07 12:02:08 -0400

    adds react range component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Slider></Slider>
+        <Range></Range>
       </div>
     );
   }
